bd84b5e738b7c14bd1b9ca1f3a65c8e05b75a9f4
John Mc Kie <john.mckie@resourcegenius.com>
2011-02-27 12:44:53 -0700

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Rge3app1::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rge3app1::Application.config.secret_token = '8cac56f2af484437fe232e9cc04f127e72308d56e9a11f5cb123e4917fa3f8622d0d7ce3d57fa510fb4facbf4e68b82354eb8a6802e4a83d50d5a42ef26b782a'
